50a0852ce73edf27620d8b94750643d5045175c4
Ariel Rodriguez <Ariels713@gmail.com>
2020-03-25 19:12:57 +0000

    changes to Appjs, savedLisjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import { Route, Link } from 'react-router-dom';
 
+// Data
 import SavedList from './Movies/SavedList';
+
+// Components
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +18,15 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+        <Link className="movieListButton" to="/">MovieList</Link>
+        <div >
+            <Route exact path="/">
+                <MovieList />
+            </Route>
+        </div>
+      <Route exact path="/MovieList/:id">
+        <Movie />
+      </Route>
     </div>
   );
 };
